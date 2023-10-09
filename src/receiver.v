b43fce5aed9d32c8e60f2cc0f92af2b92f27bc5c
Ilia Fedorenko <iliasfedorenko@mail.ru>
2023-10-09 10:16:20 +0300

    Added storing action in switching to busy mode

--- src/receiver.v
+++ src/receiver.v
@@ -26,6 +26,7 @@ module receiver #(
     integer end_col;
     integer curr_bit;
     reg parity_check;
+    reg [3:0] action_was;
 
     wire is_msg_correct = (PAR == 1) ? parity_check == rx : ~parity_check == rx;
 
@@ -76,6 +77,9 @@ module receiver #(
                         curr_col <= 0;
                         end_col <= 3;
                     end
+                    if (action == 2 || action == 3 || action == 4 || action == 5) begin
+                        action_was <= action;
+                    end
                 end
             end else begin
                 ins_clk <= (ins_clk + 1) % DIV;
@@ -85,18 +89,18 @@ module receiver #(
                     end else if ((curr_bit == W || curr_bit == W + 1) && curr_row == end_row && curr_col == end_col) begin   // one of end bit states (parity check or stop)
                         if (curr_bit == W && PAR != 0) begin    // parity_check state
                             if (is_msg_correct) begin
-                                if (action == 2) matrix[row][col] <= received[row][col];
-                                if (action == 3) begin
+                                if (action_was == 2) matrix[row][col] <= received[row][col];
+                                if (action_was == 3) begin
                                     matrix[row][0] <= received[row][0];
                                     matrix[row][1] <= received[row][1];
                                     matrix[row][2] <= received[row][2];
                                     matrix[row][3] <= received[row][3];
                                 end
-                                if (action == 4) begin
+                                if (action_was == 4) begin
                                     matrix[0][col] <= received[0][col];
                                     matrix[1][col] <= received[1][col];
                                 end
-                                if (action == 5) begin
+                                if (action_was == 5) begin
                                     matrix[0][0] <= received[0][0];
                                     matrix[0][1] <= received[0][1];
                                     matrix[0][2] <= received[0][2];
